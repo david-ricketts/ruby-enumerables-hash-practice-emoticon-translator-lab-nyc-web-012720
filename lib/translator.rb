127731a4f5e3cd675e8faee472d27f976146c67b
David Ricketts <david.ricketts119@gmail.com>
2020-01-27 16:22:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,7 +22,7 @@ end
 def get_japanese_emoticon(file_path, emoticon)
   # code goes here
   result = load_library(file_path)['get_emoticon'][emoticon]
-  
+  result ? result : "Sorry, that emoticon"
   
   
 end
