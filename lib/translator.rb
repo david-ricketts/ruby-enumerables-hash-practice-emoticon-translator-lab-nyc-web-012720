e40a7496a3930b23d352152b3f8f2d0765f76222
David Ricketts <david.ricketts119@gmail.com>
2020-01-27 16:17:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,10 +15,14 @@ def load_library
     new_hash['get_emoticon'][value[0]] = emoticons[key][1]
     new_hash['get_meaning'][value[1]] = key
   end
+  
+  new_hash
 end
 
 def get_japanese_emoticon
   # code goes here
+  
+  
 end
 
 def get_english_meaning
